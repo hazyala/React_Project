17cfd25de1ff15f0a9f35257e3921e8c70f714dc
hazyala <heamin0603@naver.com>
2025-09-23 14:16:39 +0900

    컴퍼넌트와 Props 실습 2 완성

--- src/chp05/example02/BookList.jsx
+++ src/chp05/example02/BookList.jsx
@@ -1,40 +1,60 @@
 import React from "react";
-import './BookList.css';
 import Book from "./Book";
+import './BookList.css'
 
 const books = [
     {
         title: "The Art of React",
         author: "김민주",
-        coverImage: "https://cdn.pixabay.com/photo/2021/01/21/15/54/books-5937716_1280.jpg"
+        coverImage: "https://image.yes24.com/goods/151041836/XL"
     },
     {
-        title: "Learning React",
+        title: "Learning Javascript by Myself",
         author: "윤인성",
-        coverImage: "https://cdn.pixabay.com/photo/2019/06/29/05/42/books-4305459_1280.jpg"
+        coverImage: "https://image.yes24.com/goods/105863903/XL"
     },
     {
         title: "Clean Code",
         author: "Robert C. Martin",
-        coverImage: "https://cdn.pixabay.com/photo/2023/03/08/23/21/books-7838952_1280.jpg"
+        coverImage: "https://image.yes24.com/goods/11681152/XL"
+    },
+    {
+        title: "Learning Javascript by Myself",
+        author: "윤인성",
+        coverImage: "https://image.yes24.com/goods/105863903/XL"
+    },
+    {
+        title: "Clean Code",
+        author: "Robert C. Martin",
+        coverImage: "https://image.yes24.com/goods/11681152/XL"
+    },
+    {
+        title: "Learning Javascript by Myself",
+        author: "윤인성",
+        coverImage: "https://image.yes24.com/goods/105863903/XL"
+    },
+    {
+        title: "Clean Code",
+        author: "Robert C. Martin",
+        coverImage: "https://image.yes24.com/goods/11681152/XL"
     }
 ];
 
 function BookList() {
-    return (
-      <div className={"bookListWrapper"}>
-          {
-              books.map((book) => {
-                  return (
-                    <Book
-                    title={book.title}
-                    author={book.author}
-                    coverImage={book.coverImage}
-                    />
-                  );
-              })
-          }
-      </div>
+    return(
+        <div className={"bookListWrapper"}>
+            {
+                books.map((book) =>{
+                    return(
+                        <Book
+                            title={book.title}
+                            author={book.author}
+                            coverImage={book.coverImage}
+                        />
+                    )
+                })
+            }
+        </div>
     );
 }
 
