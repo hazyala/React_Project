b3029631260377c1b48c710bb49b7e15ac585e6e
hazyala <heamin0603@naver.com>
2025-10-14 16:32:49 +0900

    9장 조건부 렌더링

--- src/index.js
+++ src/index.js
@@ -3,31 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from "./chp04/ConfirmDialog";
-import './chp04/Button.css'
-import './chp04/ConfirmDialog.css'
-import ConfirmDialogs from './chp04/ConfirmDialogs';
-import Clock from './chp04/Clock';
-import './chp04/Clock.css'
-import Comment from './chp05/exam01/Comment';
-import book from "./chp03/Book";
-import BookList from './chp05/exam02/BookList';
-import Notification from "./chp06/Notification";
-import NotificationList from "./chp06/NotificationList";
-import Counter from "./chp07/exam01/Counter";
-import Accomodate from "./chp07/Accomodate";
-import Toggle from "./chp08/Toggle";
-import Toggle_3_NewSyntax from "./chp08/Toggle_3_NewSyntax";
-import ToggleFunction from "./chp08/ToggleFunction";
-import MyButton_Argument from "./chp08/MyButton_Argument";
-import InputTest from "./chp08/InputTest";
-import ConfirmButtonFunction from "./chp08/ConfirmButtonFunction";
+
+import Greeting from "./chp09/ex1/Greeting";
+import LogControl from "./chp09/ex2/LogControl";
+import MainPageWarning from "./chp09/ex3/MainPageWarning";
+import LandingPage from "./chp09/closing_ex/LandingPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
     root.render(
         <React.StrictMode>
-            <ConfirmButtonFunction/>
+            <LandingPage />
         </React.StrictMode>
     );
 
